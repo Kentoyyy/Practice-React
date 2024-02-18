fb7f284b8d5f8c102cd3b541275c1d9af19dc7af
kentoy <kenalcantara55@gmail.com>
2024-02-18 18:01:11 +0800

    practice

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Practice Blog!</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Start</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
