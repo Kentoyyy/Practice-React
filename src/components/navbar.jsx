8bcf23a7f79a3b0ce3be67768ee974f33d3d01e9
kentoy <kenalcantara55@gmail.com>
2024-02-26 20:52:51 +0800

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import "../navbar.css";
 
 function Navbar(props) {
     return (
-        <div>
-            <h2>Hello Test</h2>
-        </div>
+        <nav className="nav">
+            <a href="#" className="nav__brand">
+                Kentoy.
+            </a>
+            <ul className="nav__menu">
+                <li className="nav__item">
+                    <a href="#" className="nav__link">Home</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">About</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Skills</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Portfolio</a></li>
+                <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
+
+            </ul>
+            <div className="nav__toggler">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+        </nav>
     );
 }
 
