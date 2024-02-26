4a5483ab6567c616a90ba8ab10c48776788c38ca
kentoy <kenalcantara55@gmail.com>
2024-02-26 21:12:31 +0800

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../navbar.css";
 
 function Navbar(props) {
+    const [active, setActive] = useState("nav__menu");
+    const navToggle = () => {
+        active === 'nav__menu' ? setActive('nav__menu nav__active') : setActive("nav__menu");
+    };
     return (
         <nav className="nav">
             <a href="#" className="nav__brand">
                 Kentoy.
             </a>
-            <ul className="nav__menu">
+            <ul className={active}>
                 <li className="nav__item">
                     <a href="#" className="nav__link">Home</a></li>
                 <li className="nav__item"><a href="#" className="nav__link">About</a></li>
@@ -16,7 +20,7 @@ function Navbar(props) {
                 <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
 
             </ul>
-            <div className="nav__toggler">
+            <div onClick = {navToggle} className="nav__toggler">
                 <div className="line1"></div>
                 <div className="line2"></div>
                 <div className="line3"></div>
