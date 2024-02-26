5fb36f119c636840be746142139c390d53134ef6
kentoy <kenalcantara55@gmail.com>
2024-02-26 20:10:24 +0800

    ups

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
+import React from 'react';
+import Navbar from './components/navbar';
 
-import Navbar from './Navbar';
-import Home from './Home';
-
-function App() {
-
-  const title = 'Welcome to the new blog!'
-
+function App(props) {
   return (
-    <div className="App">
+    <div>
       <Navbar/>
-     <div className="content">
-        <Home/>
-     </div>
     </div>
   );
 }
