1e08af8b49b7b194a9fe3525ef4d0248701b111f
kentoy <kenalcantara55@gmail.com>
2024-02-26 21:23:25 +0800

    ups

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import "../navbar.css";
 
 function Navbar(props) {
+
     const [active, setActive] = useState("nav__menu");
+    
     const navToggle = () => {
         active === 'nav__menu' ? setActive('nav__menu nav__active') : setActive("nav__menu");
-    };
+    };  
     return (
         <nav className="nav">
             <a href="#" className="nav__brand">
@@ -13,14 +15,32 @@ function Navbar(props) {
             </a>
             <ul className={active}>
                 <li className="nav__item">
-                    <a href="#" className="nav__link">Home</a></li>
-                <li className="nav__item"><a href="#" className="nav__link">About</a></li>
-                <li className="nav__item"><a href="#" className="nav__link">Skills</a></li>
-                <li className="nav__item"><a href="#" className="nav__link">Portfolio</a></li>
-                <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
-
+                    <a href="#" className="nav__link">
+                        Home
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        About
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        Skills
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        Portfolio
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        Contact
+                    </a>
+                </li>
             </ul>
-            <div onClick = {navToggle} className="nav__toggler">
+            <div onClick={navToggle} className="nav__toggler">
                 <div className="line1"></div>
                 <div className="line2"></div>
                 <div className="line3"></div>
